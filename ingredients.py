318137f9536e50a89cb4c281f6332a481d49ef45
ComputerFun1 <adoniastadesse@gmail.com>
2026-03-18 11:02:10 -0500

    Change pantry recipes to pantry items structure
    Replaced pantry recipes list with pantry items dictionary.

---
+++ ingredients.py
@@ -0,0 +1,6 @@
+PANTRY_ITEMS = {
+    "Proteins": ["Canned Tuna", "Canned Chicken", "Peanut Butter", "Dried Beans", "Canned Beans", "Lentils", "Eggs"],
+    "Grains": ["White Rice", "Brown Rice", "Pasta", "Oats", "Bread", "Quinoa", "Cornmeal"],
+    "Vegetables": ["Canned Corn", "Canned Green Beans", "Canned Tomatoes", "Potatoes", "Onions", "Carrots"],
+    "Basics": ["Olive Oil", "Salt", "Pepper", "Sugar", "Flour", "Canned Soup", "Milk Powder"]
+}
